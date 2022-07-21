f6fcb028b595dd6248f5a621d0bd4676406c21f5
Ryan <ryan-byrne@users.noreply.github.com>
2022-07-21 19:00:34 -0400

    fix configuration issue

--- raspifly/api/__init__.py
+++ raspifly/api/__init__.py
@@ -6,31 +6,28 @@ apiv1 = Blueprint('api', __name__, url_prefix='/api/v1')
 # Drone Class
 drone = Drone()
 
-_OPENAPI = "3.0.0"
-
-_INFO = {
-    "version":"0.0.1",
-    "title":"Raspifly API",
-    "license":{
-        "name":"MIT"
-    }
-}
-
 @apiv1.route('/info/', methods=['GET'])
 def info():
-		return jsonify(_INFO)
+		return jsonify({
+			"version":"0.0.1",
+			"title":"Raspifly API",
+			"license":{
+					"name":"MIT"
+			}
+		})
 
 @apiv1.route('/openapi/', methods=['GET'])
 def openapi():
-		return jsonify(_OPENAPI)
+		return "3.0.0"
 
+@apiv1.route('/configuration/', methods=['GET','POST'])
 @apiv1.route('/configuration/<setting>', methods=['GET','POST'])
-def configuration(setting=None):
-	if request.method == 'GET' and not setting:
+def configuration(setting=''):
+	if request.method == 'GET' and setting == '':
 		return jsonify(drone.configuration)
 	elif request.method == 'GET' and setting:
 		return jsonify(drone.configuration[setting])
-	elif setting:
-		return jsonify(drone.set_configuration(request.form, setting))
+	elif setting and request.method == 'POST':
+		return jsonify({"message":drone.set_configuration(request.form, setting)})
 	else:
-		return jsonify(drone.set_configuration(request.form))
+		return jsonify({"message":drone.set_configuration(request.form)})
